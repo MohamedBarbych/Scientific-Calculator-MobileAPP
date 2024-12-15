29e0f7209dc8b76d560f209f45448599e9f2edd7
MohamedBarbych <MohamedBarbych@users.noreply.github.com>
2024-12-15 21:16:39 +0100

    Ajout de [description de la modifications]

--- app/src/androidTest/java/com/fstt/calculatrice_scientifique/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/fstt/calculatrice_scientifique/ExampleInstrumentedTest.java
@@ -1,7 +1,7 @@
 package com.fstt.calculatrice_scientifique;
 
 import android.content.Context;
-
+//Test successfully passed !!
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
